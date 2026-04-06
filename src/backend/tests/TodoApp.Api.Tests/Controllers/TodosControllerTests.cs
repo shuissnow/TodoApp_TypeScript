e2d1e99342a606f6cc3f9470883dcd0f452a11eb
shuissnow <42664689+shuissnow@users.noreply.github.com>
2026-04-06 10:15:49 +0900

    [chore]backendのCI確認用@V2

--- src/backend/tests/TodoApp.Api.Tests/Controllers/TodosControllerTests.cs
+++ src/backend/tests/TodoApp.Api.Tests/Controllers/TodosControllerTests.cs
@@ -6,7 +6,7 @@ using TodoApp.Api.Models;
 using TodoApp.Api.Services;
 
 namespace TodoApp.Api.Tests.Controllers;
-// CI動作確認用
+// CI動作確認用V2
 public class TodosControllerTests
 {
     private readonly Mock<ITodoService> _serviceMock = new();
